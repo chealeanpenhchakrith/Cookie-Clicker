d8f7dab2d4b5d18301c591fab1b0dc139b87dfa9
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-08 17:17:38 +0200

    Add 3 automatic production offers

--- Cookie-Clicker/src/App.vue
+++ Cookie-Clicker/src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <cookieComponent />
+  <div class="flex justify-center">
+    <div class="flex flex-col items-center">
+      <div class="mt-30">
+        <h1 class="text-6xl flex justify-center pb-10 text-amber-600">Cookie Clicker</h1>
+        <cookieComponent />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
--- Cookie-Clicker/src/components/icons/cookieComponent.vue
+++ Cookie-Clicker/src/components/icons/cookieComponent.vue
@@ -8,15 +8,45 @@
       height="500"
     />
   </div>
-  <div v-if="cookieCounter != 0">
-    <h1>Cookie generated : {{ cookieCounter }}</h1>
-  </div>
-  <div v-else>
-    <h1>0 cookie generated, click on the cookie</h1>
-  </div>
-  <div v-if="cookieCounter > 10">
-    <h1>Generate 1 cookie every 10 seconds</h1>
-    <button @click="autoProduction">Buy for 10 cookies</button>
+  <div class="flex flex-col">
+    <div v-if="cookieCounter != 0">
+      <h1 class="text-2xl flex justify-center">Cookie generated : {{ cookieCounter }}</h1>
+    </div>
+    <div v-else>
+      <h1 class="text-2xl flex justify-center">Click On The Cookie</h1>
+    </div>
+    <div class="flex flex-col">
+      <div v-if="cookieCounter >= 10">
+        <div class="flex justify-center">
+          <button
+            class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-15"
+            @click="autoProduction5"
+          >
+            5 cookies every 10s (cost 5 cookies)
+          </button>
+        </div>
+      </div>
+      <div v-if="cookieCounter >= 20">
+        <div class="flex justify-center">
+          <button
+            class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-5"
+            @click="autoProduction10"
+          >
+            10 cookies every 5s (cost 10 cookies)
+          </button>
+        </div>
+      </div>
+      <div v-if="cookieCounter >= 30">
+        <div class="flex justify-center">
+          <button
+            class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-5"
+            @click="autoProduction30"
+          >
+            30 cookies every 3s (cost 30 cookies)
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,10 +57,24 @@ function incrementCookie() {
   cookieCounter.value += 1
 }
 
-function autoProduction() {
-  cookieCounter.value = cookieCounter.value - 10
+function autoProduction5() {
+  cookieCounter.value = cookieCounter.value - 5
   setInterval(() => {
     cookieCounter.value += 1
   }, 10000)
+}
+
+function autoProduction10() {
+  cookieCounter.value = cookieCounter.value - 10
+  setInterval(() => {
+    cookieCounter.value += 10
+  }, 5000)
+}
+
+function autoProduction30() {
+  cookieCounter.value = cookieCounter.value - 30
+  setInterval(() => {
+    cookieCounter.value += 30
+  }, 3000)
 }
 </script>
