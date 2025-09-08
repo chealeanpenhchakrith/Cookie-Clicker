debe4f324950150e125e413b41fc541d486b7fd9
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-08 23:24:59 +0200

    Add 3 special features (upgrade options, success achievement message, save user cookie's data)

--- Cookie-Clicker/src/App.vue
+++ Cookie-Clicker/src/App.vue
@@ -1,16 +1,9 @@
 <template>
-  <div class="flex justify-center">
-    <div class="flex flex-col items-center">
-      <div class="mt-30">
-        <h1 class="text-6xl flex justify-center pb-10 text-amber-600">Cookie Clicker</h1>
-        <cookieComponent />
-      </div>
-    </div>
-  </div>
+  <cookieComponent />
 </template>
 
 <script setup>
-import cookieComponent from './components/icons/cookieComponent.vue'
+import cookieComponent from './components/icons/cookieComponent.vue';
 </script>
 
 <style scoped></style>
--- Cookie-Clicker/src/components/icons/cookieComponent.vue
+++ Cookie-Clicker/src/components/icons/cookieComponent.vue
@@ -1,80 +1,92 @@
 <template>
-  <div>
-    <img
-      @click="incrementCookie"
-      src="/Users/rithchea/Desktop/Academic Project/Cookie-Clicker/Cookie-Clicker/src/assets/Cookie.jpg"
-      alt="cookie"
-      width="500"
-      height="500"
-    />
-  </div>
-  <div class="flex flex-col">
-    <div v-if="cookieCounter != 0">
-      <h1 class="text-2xl flex justify-center">Cookie generated : {{ cookieCounter }}</h1>
+  <div class="flex flex-row justify-center mt-40">
+    <div class="flex flex-col items-center">
+      <h1 class="text-4xl text-amber-900">Cookie Clicker</h1>
+      <img
+        @click="incrementCookie"
+        src="/Users/rithchea/Desktop/Academic Project/Cookie-Clicker/Cookie-Clicker/src/assets/Cookie.jpg"
+        width="450"
+        height="450"
+        alt="cookie"
+      />
+      <h1 class="text-2xl" v-if="gameStart === false">Click on the cookie to play !</h1>
+      <h1 class="text-2xl" v-if="gameStart === true">
+        You produced {{ cookieCounter }} cookie<span v-if="cookieCounter >= 2">s</span>
+      </h1>
     </div>
-    <div v-else>
-      <h1 class="text-2xl flex justify-center">Click On The Cookie</h1>
-    </div>
-    <div class="flex flex-col">
-      <div v-if="cookieCounter >= 10">
-        <div class="flex justify-center">
-          <button
-            class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-15"
-            @click="autoProduction5"
-          >
-            5 cookies every 10s (cost 5 cookies)
-          </button>
-        </div>
-      </div>
-      <div v-if="cookieCounter >= 20">
-        <div class="flex justify-center">
-          <button
-            class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-5"
-            @click="autoProduction10"
-          >
-            10 cookies every 5s (cost 10 cookies)
-          </button>
-        </div>
-      </div>
-      <div v-if="cookieCounter >= 30">
-        <div class="flex justify-center">
-          <button
-            class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-5"
-            @click="autoProduction30"
-          >
-            30 cookies every 3s (cost 30 cookies)
-          </button>
-        </div>
+    <div class="flex flex-col gap-1.5">
+      <button
+        class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        @click="upgrade1Cookie"
+        v-if="cookieCounter >= 10"
+      >
+        Upgrade +1/2s (Cost 10 Cookies)
+      </button>
+      <button
+        class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        @click="upgrade2Cookie"
+        v-if="cookieCounter >= 30"
+      >
+        Upgrade +3/2s (Cost 30 Cookies)
+      </button>
+      <button
+        class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        @click="upgrade3Cookie"
+        v-if="cookieCounter >= 75"
+      >
+        Upgrade +10/2s (Cost 75 Cookies)
+      </button>
+      <div>
+        <h1 class="text-2xl text-orange-700" v-if="cookieCounter >= 50">
+          Congratulations for your 50 produced cookies !
+        </h1>
+        <h1 class="text-2xl text-orange-700" v-if="cookieCounter >= 100">
+          Congratulations for your 100 produced cookies !
+        </h1>
+        <h1 class="text-2xl text-orange-700" v-if="cookieCounter >= 150">
+          Congratulations for your 150 produced cookies !
+        </h1>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch, onMounted } from 'vue'
+const gameStart = ref(false)
 const cookieCounter = ref(0)
-function incrementCookie() {
+
+onMounted(() => {
+  const savedCookies = localStorage.getItem('cookieCounter')
+  if (savedCookies !== null) {
+    cookieCounter.value = parseInt(savedCookies)
+  }
+})
+
+watch(cookieCounter, (newVal) => {
+  localStorage.setItem('cookieCounter', newVal)
+})
+
+const incrementCookie = () => {
+  gameStart.value = true
   cookieCounter.value += 1
 }
-
-function autoProduction5() {
-  cookieCounter.value = cookieCounter.value - 5
+const upgrade1Cookie = () => {
+  cookieCounter.value -= 10
   setInterval(() => {
     cookieCounter.value += 1
-  }, 10000)
+  }, 2000)
 }
-
-function autoProduction10() {
-  cookieCounter.value = cookieCounter.value - 10
+const upgrade2Cookie = () => {
+  cookieCounter.value -= 30
+  setInterval(() => {
+    cookieCounter.value += 3
+  }, 2000)
+}
+const upgrade3Cookie = () => {
+  cookieCounter.value -= 75
   setInterval(() => {
     cookieCounter.value += 10
-  }, 5000)
-}
-
-function autoProduction30() {
-  cookieCounter.value = cookieCounter.value - 30
-  setInterval(() => {
-    cookieCounter.value += 30
-  }, 3000)
+  }, 2000)
 }
 </script>
